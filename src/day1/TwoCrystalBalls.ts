d9ad454b4148ed41e0b8a053328b42593d82396f
Ben Baron <benjamin.m.baron@gmail.com>
2024-03-30 16:33:38 +0700

    solved find first point when a array of false becomes true

---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,16 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpAmount;
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+  let j = i - jumpAmount;
+  for (; j < breaks.length; j++) {
+    if (breaks[j]) {
+      return j;
+    }
+  }
+  return -1;
+}
